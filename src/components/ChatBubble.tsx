4e7e31973f73e4f519a3dc63dcd5c8144866f7d0
sasha solano <sashasolano@gmail.com>
2026-01-12 11:48:59 -0400

    corrigiendo error

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -11,7 +11,7 @@ const ChatBubble: React.FC<ChatBubbleProps> = ({ children, className }) => {
       {children}
       <div className="absolute left-0 top-0 w-0 h-0 border-t-[5px] border-t-transparent border-b-[5px] border-b-transparent border-r-[15px] border-r-green-500 transform -translate-x-full bg-[#99DF63]  z-10"></div>
     </div>
-  ;
+  );
 };
 
 export default ChatBubble;
