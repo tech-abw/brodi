175fa94279256252309a73a402a54862b1fd90be
Charlie Serrano <charlie@abw.mx>
2026-01-16 18:27:09 -0600

    feat: Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone', // <--- ESTO ES VITAL
+    // tus otras configuraciones...
+};
+
+module.exports = nextConfig;
